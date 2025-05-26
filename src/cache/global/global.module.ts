f261598c092b14dc63531df4150fbd99b6bfcae4
malaz-ahmad <lozo4hk@gmail.com>
2025-05-27 00:55:27 +0300

    commit cache

---
+++ src/cache/global/global.module.ts
@@ -0,0 +1,29 @@
+import { Global, Module } from '@nestjs/common';
+import { CacheModule } from '@nestjs/cache-manager';
+import { createKeyv } from '@keyv/redis';
+import Keyv from 'keyv';
+import { CacheableMemory } from 'cacheable';
+import { ConfigService } from '@nestjs/config';
+
+@Global()
+@Module({
+  imports: [
+    CacheModule.registerAsync({
+      isGlobal: true,
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => {
+        return {
+          ttl: 6 * 1000, //redis
+          stores: [
+            new Keyv({
+              store: new CacheableMemory({ ttl: 4 * 1000, lruSize: 5000 }), //cache
+            }),
+            createKeyv(config.get<string>('REDIS')),
+          ],
+        };
+      },
+    }),
+  ],
+  exports: [CacheModule],
+})
+export class GlobalCacheModule {}

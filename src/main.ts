88f5ed44584d310130c315c3dfc6f053e768fa21
malaz-ahmad <lozo4hk@gmail.com>
2025-05-31 21:40:39 +0300

    main enable Cors "Patch"

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   //لا تعدل على جسم الطلب
   app.use('/webhook/stripe', express.raw({ type: 'application/json' }));
-  
+
   // get X-Forwarded-For when I use proxy  I didn't use true for range limiting
   app.getHttpAdapter().getInstance().set('trust proxy', 'loopback');
   //X-Frame-Options: DENY
@@ -20,7 +20,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: 'http://localhost:5173',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
